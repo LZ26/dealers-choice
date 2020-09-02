ebc5565a1ebe2a03da49725da5672e46540b2b1c
Laziz <laziz.zokir26@gmail.com>
2020-09-02 02:00:34 -0400

    fixed issues with index.html

--- server/index.js
+++ server/index.js
@@ -6,11 +6,10 @@ const db = require('./db');
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.use('/api', require('./routes/index.js'));
