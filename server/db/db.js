03b1f2a25f8dd37fcb5cd6590b27c3c38d7cb896
Laziz <laziz.zokir26@gmail.com>
2020-09-02 02:07:31 -0400

    fixed db issues

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/dota'
+  process.env.DATABASE_URL || 'postgres://localhost:5432/dota'
 );
 
 module.exports = db;
